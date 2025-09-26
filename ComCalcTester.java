a9611fad0861e3235a8c155605a611f572210824
Isa.C <isacain06@gmail.com>
2025-09-25 21:14:26 -0500

    Update and rename Notes to ComCalcTester.java
    pt 1/5

---
+++ ComCalcTester.java
@@ -0,0 +1,24 @@
+
+public class Driver {
+
+ 
+
+       public static void main(String[] args)
+
+       {
+
+             // Create commission calculator
+
+             CommissionCalculator calc = new CommissionCalculator();
+
+            
+
+             // Run commission calculator
+
+             calc.Run();
+
+       }
+
+ 
+
+}
